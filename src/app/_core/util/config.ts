d445325818d4264e650573f81a8da7b2486953a8
huonguy <uthuong125@gmail.com>
2022-08-08 11:33:28 +0700

    update changes - config

--- src/app/_core/util/config.ts
+++ src/app/_core/util/config.ts
@@ -1,10 +1,10 @@
-export const TOKEN_CYBERSOFT: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzAiLCJIZXRIYW5TdHJpbmciOiIxNC8xMC8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NjU3MDU2MDAwMDAiLCJuYmYiOjE2Mzc0Mjc2MDAsImV4cCI6MTY2NTg1MzIwMH0.RAzH9H37ZyQ8ZT6A62fw3_bDfJOCq0A9vz08qT262EU';
+// export const TOKEN_CYBERSOFT: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzAiLCJIZXRIYW5TdHJpbmciOiIxNC8xMC8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NjU3MDU2MDAwMDAiLCJuYmYiOjE2Mzc0Mjc2MDAsImV4cCI6MTY2NTg1MzIwMH0.RAzH9H37ZyQ8ZT6A62fw3_bDfJOCq0A9vz08qT262EU';
 export const ACCESS_TOKEN: string = 'accessToken';
 export const USER_LOGIN: string = 'userLogin';
 
 //domain backend
 // export const DOMAIN = "https://airbnb.cybersoft.edu.vn";
-export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://airbnb-api-huong.herokuapp.com";
 //tslint
 
 
